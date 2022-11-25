dfc74d8623c3c2c4eee48bca657fb5d6d68f10b0
Luiz Felipe Canário Costa <luizfelipecanariocosta@gmail.com>
2022-11-25 14:03:17 -0300

    .

---
+++ C#/CalculoSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double preço1, preço2, total;
+        int codigo1, codigo2, quantidade1, quantidade2;
+
+        string[] valores = Console.ReadLine().Split(' ');
+
+        codigo1 = int.Parse(valores[0]);
+        quantidade1 = int.Parse(valores[1]);
+        preço1 = double.Parse(valores[2]);
+
+        valores = Console.ReadLine().Split(' ');
+
+        codigo2 = int.Parse(valores[0]);
+        quantidade2 = int.Parse(valores[1]);
+        preço2 = double.Parse(valores[2]);
+
+        total = ((preço1 * quantidade1) + (preço2 * quantidade2));
+
+        Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2"));
+
+
+    }
+
+}
---
+++ C#/Esfera/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double raio, volume, pi = 3.14159;
+
+        string[] valores = Console.ReadLine().Split(' ');
+
+        raio = double.Parse(valores[0]);
+        volume = ((4.0 / 3.0) * pi * (raio * raio * raio));
+
+        Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+
+
+
+    }
+
+}
